39b87ad6ffb3ec24e158885dec69733a011238af
superPantsXXXIII <124884690+superPantsXXXIII@users.noreply.github.com>
2023-07-26 11:14:25 +0300

    Add files via upload

---
+++ events.js
@@ -0,0 +1,195 @@
+const express = require("express");
+const router = express.Router();
+const { Subject } = require("rxjs")
+const queue = new Subject();
+queueSub();
+const sqlCon = require("../db/sqlConnect");
+const { auth } = require("../middleware/auth");
+const { validateEvent } = require("../models/eventModel");
+const { sendRequestToHost } = require("../middleware/sendGrid")
+
+router.get("/", async (req, res) => {
+    const strSql = `SELECT *,(SELECT count(*) FROM users_events WHERE event_id = events.event_id) current_particepants FROM events `;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err); }
+        res.json(results);
+    })
+})
+
+router.get("/forShow", async (req, res) => {
+    const strSql = `SELECT events.event_id,title,city,adress,description,date_created,event_date,max_paticipants,(SELECT count(*) FROM users_events WHERE event_id = events.event_id) current_particepants,user_id FROM events,users_events where events.event_id = users_events.event_id`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err); }
+        res.json(results);
+    })
+})
+
+router.get("/single/:event_id", async (req, res) => {
+    const event_id = Number(req.params.event_id);
+    const strSql = `SELECT *,(SELECT count(*) FROM users_events where event_id=${event_id}) current_paticipants FROM events where event_id=${event_id}`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err); }
+        res.json(results);
+    })
+})
+
+router.get("/describe", async (req, res) => {
+    const strSql = `DESCRIBE events `;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.get("/users", async (req, res) => {
+    const strSql = `SELECT * FROM users_events `;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.get("/users/count/:event_id", async (req, res) => {
+    const event_id = Number(req.params.event_id);
+    const strSql = `SELECT count(*) current_paticipants,(SELECT max_paticipants from events  where event_id=${event_id}) max_paticipants FROM users_events where event_id=${event_id}`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.get("/users/getAllMyEvents/:user_id", async (req, res) => {
+    const user_id = Number(req.params.user_id);
+    const strSql = `SELECT * FROM events where event_id in (SELECT event_id FROM users_events where user_id=${user_id})`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.get("/users/getParticipants/:event_id", async (req, res) => {
+    const event_id = Number(req.params.event_id);
+    const strSql = `SELECT name,email,approved FROM users_events,users where event_id =${event_id} and host = 0 and users.user_id = users_events.user_id `;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.get("/users/describe", async (req, res) => {
+    const strSql = `DESCRIBE users_events `;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.post("/", auth, async (req, res) => {
+    const validBody = validateEvent(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    let strSql = `INSERT INTO events VALUES (null,?,?,?,?,?,?,?)`;
+    let { title, city, adress, description, event_date, max_paticipants } = req.body;
+    sqlCon.query(strSql, [title, city, adress, description, (new Date).toJSON().slice(0, 10), event_date, max_paticipants], (err, results) => {
+        if (err) {
+            return res.status(400).json(err);
+        }
+        // res.status(201).json(results);
+        strSql = `INSERT INTO users_events VALUES (?,?,?,?)`;
+        sqlCon.query(strSql, [req.tokenData.user_id, results.insertId, 1, 1], (err, results) => {
+            if (err) {
+                return res.status(400).json(err);
+            }
+            res.status(201).json(results);
+        })
+    })
+})
+
+router.post("/joinEvent/:event_id", auth, async (req, res) => {
+    let event_id = Number(req.params.event_id);
+    let strSql = `SELECT count(*) current_paticipants,(SELECT max_paticipants from events  where event_id=${event_id}) max_paticipants FROM users_events where event_id=${event_id}`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        if (results[0] && results[0].current_paticipants < results[0].max_paticipants) {
+            strSql = `INSERT INTO users_events VALUES (?,?,?,?)`;
+            sqlCon.query(strSql, [req.tokenData.user_id, event_id, 0, 0], (err, results) => {
+                if (err) { return res.json(err); }
+                res.json(results);
+            })
+            queue.next(event_id)
+        }
+        else {
+            res.json({ err: "event maxed out or doesnt exist" });
+        }
+    })
+})
+
+async function queueSub() {
+    queue.subscribe((event_id) => {
+        let strSql = `select email from users,users_events where event_id = ${event_id} and users.user_id = users_events.user_id`;
+        sqlCon.query(strSql, (err, results) => {
+            try {
+                sendRequestToHost(results);
+            }
+            catch (err) {
+                console.log(err)
+            }
+        })
+    })
+}
+
+router.put("/:event_id", async (req, res) => {
+    const validBody = validateEvent(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    const event_id = Number(req.params.event_id);
+    const strSql = `Update events set title=?,city=?,adress=?,description=?,event_date=?,max_paticipants=? where  event_id = ${event_id}`;
+    let { title, city, adress, description, event_date, max_paticipants } = req.body;
+    sqlCon.query(strSql, [title, city, adress, description, event_date, max_paticipants], (err, results) => {
+        if (err) {
+            return res.status(400).json(err);
+        }
+        res.status(201).json(results);
+    })
+})
+
+router.patch("/users/approve", async (req, res) => {
+    const user_id = Number(req.body.user_id);
+    const event_id = Number(req.body.event_id);
+    const strSql = `Update users_events set approved=1 where user_id = ${user_id} and event_id = ${event_id}`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err) }
+        res.json(results)
+    })
+})
+
+router.delete("/:event_id", auth, async (req, res) => {
+    const event_id = Number(req.params.event_id);
+    let strSql = `select host from users_events where event_id = ${event_id} and user_id = ${req.tokenData.user_id}`;
+    sqlCon.query(strSql, (err, results) => {
+        if (err) { return res.json(err); }
+        if (results[0] && results[0].host == 1 || req.tokenData.role == "admin") {
+            strSql = `Delete from users_events where event_id = ${event_id}`;
+            sqlCon.query(strSql, (err, results) => {
+                if (err) { return res.json(err); }
+                strSql = `Delete from events where event_id = ${event_id}`;
+                sqlCon.query(strSql, (err, results) => {
+                    if (err) { return res.json(err); }
+                    res.json(results);
+                })
+            })
+        }
+        else {
+            res.json({ err: "unauthorized request" });
+        }
+    })
+})
+
+router.get("/testing_sg", async (req, res) => {
+    testSendTemplated();
+    return res.json("email sent")
+})
+
+module.exports = router;
---
+++ sendGrid.js
@@ -0,0 +1,42 @@
+const sgMail = require('@sendgrid/mail')
+const {config} = require('../secret/config')
+sgMail.setApiKey(config.sg_api_key)
+
+exports.testSend = () =>{
+    const msg = {
+        to: config.sg_sender, // Change to your recipient
+        from: config.sg_sender, // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    
+    sgMail.send(msg).then((response) => {
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+    }).catch((error) => {
+        console.error(error)
+    })
+}
+
+exports.sendRequestToHost = (email) =>{
+    const msg = {
+        to: config.sg_sender,
+        from: config.sg_sender,
+        templateId: 'd-daed453466674ebd8326832b61e3f845',
+        dynamicTemplateData: {
+          title: 'meet and eat',
+          name: 'shay',
+          email,
+        },
+      };
+    
+    sgMail.send(msg).then((response) => {
+        console.log(response[0].statusCode)
+        console.log(response[0].headers)
+    }).catch((error) => {
+        console.error(error)
+    })
+}
+
+	
