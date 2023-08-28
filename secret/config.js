cbc8f53a8da83928f16f38c6e7c41a3fb164e0e7
superPantsXXXIII <124884690+superPantsXXXIII@users.noreply.github.com>
2023-08-28 15:22:51 +0300

    Add files via upload

--- secret/config.js
+++ secret/config.js
@@ -5,5 +5,8 @@ exports.config = {
     user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE,   
-    secret:process.env.SECRET   
+    secret:process.env.SECRET,
+    sg_api_key:process.env.SENDGRID_API_KEY,
+    sg_sender:process.env.SENDGREID_SENDER,
+    novu_api_key:process.env.NOVU_API_KEY
 }
