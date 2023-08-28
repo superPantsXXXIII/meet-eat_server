2143def9e11494daf39cb0793796e040ad7ae074
superPantsXXXIII <124884690+superPantsXXXIII@users.noreply.github.com>
2023-08-28 12:20:55 +0300

    Add files via upload

--- routes/events.js
+++ routes/events.js
@@ -10,7 +10,8 @@ const novu = require("../middleware/notification")
 queueSub();
 
 router.get("/", async (req, res) => {
-    const strSql = `SELECT *,(SELECT count(*) FROM users_events WHERE event_id = events.event_id) current_particepants FROM events order by event_date`;
+    const orderDate = req.query.date?"order by event_date":"";
+    const strSql = `SELECT *,(SELECT count(*) FROM users_events WHERE event_id = events.event_id) current_particepants FROM events where event_date > now() ${orderDate}`;
     sqlCon.query(strSql, (err, results) => {
         if (err) { return res.json(err); }
         res.json(results);
@@ -20,7 +21,7 @@ router.get("/", async (req, res) => {
 router.get("/loggedIn/:user_id", async (req, res) => {
     const user_id = Number(req.params.user_id);
     const orderDate = req.query.date?"order by event_date":"";
-    const strSql = `SELECT *,(SELECT count(*) FROM users_events WHERE event_id = events.event_id) current_particepants FROM events where event_id not in (select event_id from users_events where user_id = ${user_id}) ${orderDate}`;
+    const strSql = `SELECT *,(SELECT count(*) FROM users_events WHERE event_id = events.event_id) current_particepants FROM events where event_id not in (select event_id from users_events where user_id = ${user_id}) and event_date > now() ${orderDate}`;
     sqlCon.query(strSql, (err, results) => {
         if (err) { return res.json(err); }
         res.json(results);
